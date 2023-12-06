5b58445949d2766a2670bb5e9d9b89a32d37f889
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-12-06 15:52:09 +0700

    initial commit

---
+++ pages/api/offeredCourses.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+export default async function handler(request, response) {
+  try {
+    const client = await new MongoClient(uri, options).connect();
+    console.log("Just Connected to MongoDB");
+    const db = client.db("offeredCourses-db");
+    const collection = db.collection("offeredCourses-collection");
+    const results = await collection.find({}).toArray();
+    response.status(200).json(results);
+    // response.status is used to set the HTTP status code, and json is used to send a JSON response.
+  } catch (e) {
+    console.error("Error connecting to MongoDB:", e);
+    response.status(500).json(e);
+  }
+}
